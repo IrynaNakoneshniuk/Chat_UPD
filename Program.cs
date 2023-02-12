9aab345f48ab88de3ab4910beec26510ad012d0e
IrynaNakoneshniuk <103773315+IrynaNakoneshniuk@users.noreply.github.com>
2023-02-12 08:34:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Chat_UPD
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Write("Введіть ім'я: ");
+            string? user = Console.ReadLine();
+            Upd_client upd_Client = new Upd_client(user);
+            Task.Run(upd_Client.ReceiveMessageAsync);
+            await upd_Client.SendMessageAsync();
+        }
+    }
+}
---
+++ Upd_client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_UPD
+{
+    public class Upd_client
+    {
+        public int localPort = 8001;
+        public IPAddress brodcastAddress = IPAddress.Parse("224.0.0.0");
+        public string? username = null;
+
+        public Upd_client(string? username)
+        {
+            this.username = username;
+        }
+        public async Task SendMessageAsync()
+        {
+            using var sender = new UdpClient(); 
+            while (true)
+            {
+                string? message = Console.ReadLine(); 
+                if (string.IsNullOrWhiteSpace(message)) break;
+
+                message = $"{username}: {message}";
+                byte[] data = Encoding.Unicode.GetBytes(message);
+
+                await sender.SendAsync(data, new IPEndPoint(brodcastAddress, localPort));
+            }
+        }
+        public async Task ReceiveMessageAsync()
+        {
+            using var receiver = new UdpClient(localPort); 
+
+            receiver.JoinMulticastGroup(brodcastAddress);
+            while (true)
+            {
+                var result = await receiver.ReceiveAsync();
+                string message = Encoding.Unicode.GetString(result.Buffer);
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
